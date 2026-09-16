cad48ae440331a1cf475b04c2438c3446937fcee
usrbinbryce <197551681+usrbinbryce@users.noreply.github.com>
2026-09-15 20:39:07 -0500

    fix(json): add doc comments

--- internal/json/json.go
+++ internal/json/json.go
@@ -9,6 +9,8 @@ type errorResponse struct {
 	Error string `json:"error"`
 }
 
+// Reads a JSON request into a given data structure.
+// This function will return an error if it fails.
 func Read(r *http.Request, data any) error {
 	decoder := json.NewDecoder(r.Body)
 	decoder.DisallowUnknownFields()
@@ -16,12 +18,16 @@ func Read(r *http.Request, data any) error {
 	return decoder.Decode(data)
 }
 
+// Writes a JSON response to a given http.ResponseWriter
+// with the provided status code and data structure.
 func Write(w http.ResponseWriter, status int, data any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(data)
 }
 
+// Writes a JSON error response to a given http.ResponseWriter
+// with the provided status code and message
 func WriteError(w http.ResponseWriter, status int, msg string) {
 	resp := errorResponse{
 		Error: msg,
