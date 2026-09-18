66eca05f36483802b5c465877d77d10e469f4165
usrbinbryce <197551681+usrbinbryce@users.noreply.github.com>
2026-09-17 21:53:23 -0500

    feat(middleware): security headers
    Added:
    - Security middleware to apply headers for better request security

---
+++ internal/middleware/middleware.go
@@ -0,0 +1,15 @@
+package middleware
+
+import "net/http"
+
+func SecurityHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "DENY")
+		w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
+		w.Header().Set("Permissions-Policy", "geolocation=(), camera=(), microphone=()")
+		w.Header().Set("Content-Security-Policy", "default-src 'none'")
+		w.Header().Set("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
+		next.ServeHTTP(w, r)
+	})
+}
